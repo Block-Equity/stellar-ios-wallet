dcf55904ef75570d5cf145af25784ea4faa70020
Satraj Bambra <satraj.bambra@me.com>
2018-05-28 00:18:23 -0400

    Added order book views.

--- BlockEQ/Utils/Constants.swift
+++ BlockEQ/Utils/Constants.swift
@@ -16,7 +16,7 @@ struct Colors {
     static let secondaryDarkTransparent =  UIColor(red: 7.0/255.0, green: 35.0/255.0, blue: 122.0/255.0, alpha: Alphas.transparent)
     static let tertiaryDark =  UIColor(red: 0.0/255.0, green: 132.0/255.0, blue: 255.0/255.0, alpha: Alphas.opaque)
     static let tertiaryDarkTransparent =  UIColor(red: 0.0/255.0, green: 132.0/255.0, blue: 255.0/255.0, alpha: Alphas.transparent)
-    static let lightBackground = UIColor(red: 250.0/255.0, green: 250.0/255.0, blue: 250.0/255.0, alpha: Alphas.opaque)
+    static let lightBackground = UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: Alphas.opaque)
     static let lightBlue = UIColor(red: 247.0/255.0, green: 249.0/255.0, blue: 253.0/255.0, alpha: Alphas.opaque)
     static let darkGray = UIColor(red: 74.0/255.0, green: 74.0/255.0, blue: 74.0/255.0, alpha: Alphas.opaque)
     static let darkGrayTransparent = UIColor(red: 74.0/255.0, green: 74.0/255.0, blue: 74.0/255.0, alpha: Alphas.semiTransparent)
---
+++ BlockEQ/Views/Cells/OrderBookCell.swift
@@ -0,0 +1,26 @@
+//
+//  OrderBookCell.swift
+//  BlockEQ
+//
+//  Created by Satraj Bambra on 2018-05-27.
+//  Copyright © 2018 Satraj Bambra. All rights reserved.
+//
+
+import UIKit
+
+class OrderBookCell: UITableViewCell {
+    @IBOutlet weak var option1Label: UILabel!
+    @IBOutlet weak var option2Label: UILabel!
+    @IBOutlet weak var option3Label: UILabel!
+    
+    static let cellIdentifier = "OrderBookCell"
+    static let rowHeight: CGFloat = 40.0
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        option1Label.textColor = Colors.darkGray
+        option2Label.textColor = Colors.darkGray
+        option3Label.textColor = Colors.darkGray
+    }
+}
