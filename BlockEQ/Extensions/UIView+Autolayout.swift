fcf77d9a1560544d92a80d5c2cbe34dedf1cfc0e
Nick DiZazzo <nick@twg.io>
2018-09-27 16:53:29 -0400

    [FEATURE] Adds support to display items in the "About" section

---
+++ BlockEQ/Extensions/UIView+Autolayout.swift
@@ -0,0 +1,86 @@
+//
+//  UIView+Autolayout.swift
+//  BlockEQ
+//
+//  Created by Nick DiZazzo on 2018-09-28.
+//  Copyright © 2018 Satraj Bambra. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+    func constraintsForViewToAllEdges(
+        _ view: UIView,
+        insets: UIEdgeInsets = .zero
+        ) -> [NSLayoutConstraint] {
+        let edges: [NSLayoutConstraint.Attribute] = [.leading, .trailing, .top, .bottom]
+        return self.constraintsForView(view, edges: edges, insets: insets)
+    }
+
+    func constrainViewToAllEdges(_ view: UIView, insets: UIEdgeInsets = .zero) {
+        let edges: [NSLayoutConstraint.Attribute] = [.leading, .trailing, .top, .bottom]
+        self.constrainView(view, edges: edges, insets: insets)
+    }
+
+    func constrainView(
+        _ view: UIView,
+        edges: [NSLayoutConstraint.Attribute],
+        insets: UIEdgeInsets = .zero,
+        priority: UILayoutPriority? = nil
+        ) {
+
+        let constraints = self.constraintsForView(
+            view,
+            edges: edges,
+            insets: insets,
+            priority: priority
+        )
+        self.addConstraints(constraints)
+    }
+
+    func constraintsForView(
+        _ view: UIView,
+        edges: [NSLayoutConstraint.Attribute],
+        insets: UIEdgeInsets,
+        priority: UILayoutPriority? = nil
+        ) -> [NSLayoutConstraint] {
+        return edges.map({ (edge) -> NSLayoutConstraint in
+            return self.constrainView(view, toEdge: edge, insets: insets, priority: priority)
+        })
+    }
+
+    func constrainView(
+        _ view: UIView,
+        toEdge edge: NSLayoutConstraint.Attribute,
+        insets: UIEdgeInsets = .zero,
+        priority: UILayoutPriority?
+        ) -> NSLayoutConstraint {
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        let inset: CGFloat = {
+            switch edge {
+            case .leading: return insets.left
+            case .trailing: return -1 * insets.right
+            case .bottom: return -1 * insets.bottom
+            case .top: return insets.top
+            default: return 0
+            }
+        }()
+
+        let constraint = NSLayoutConstraint(
+            item: view,
+            attribute: edge,
+            relatedBy: .equal,
+            toItem: self,
+            attribute: edge,
+            multiplier: 1,
+            constant: inset
+        )
+
+        if let priority = priority {
+            constraint.priority = priority
+        }
+        return constraint
+    }
+}
---
+++ BlockEQ/View Controllers/Misc/WebViewController.swift
@@ -0,0 +1,61 @@
+//
+//  WebViewController.swift
+//  Uphabit
+//
+//  Created by Nick DiZazzo on 2018-09-28.
+//  Copyright © 2018 BlockEQ. All rights reserved.
+//
+
+import Foundation
+
+class WebViewController: UIViewController {
+
+    let url: URL
+    let webView = UIWebView()
+    let pageTitle: String
+
+    init(url: URL, pageTitle: String) {
+        self.url = url
+        self.pageTitle = pageTitle
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.url = BlockEQURL.site.url
+        self.pageTitle = "BLOCKEQ_WEBSITE".localized()
+        super.init(coder: aDecoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBar()
+        setupWebView()
+        setupConstraints()
+    }
+
+    private func setupNavBar() {
+        navigationItem.title = pageTitle
+        navigationItem.backBarButtonItem = nil
+    }
+
+    private func setupWebView() {
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.delegate = self
+        view.addSubview(webView)
+
+        let request = URLRequest(url: url)
+        webView.loadRequest(request)
+    }
+
+    private func setupConstraints() {
+        view.constrainViewToAllEdges(webView)
+    }
+}
+
+extension WebViewController: UIWebViewDelegate {
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        UIAlertController.simpleError(title: "ERROR_TITLE".localized(),
+                                      message: error.localizedDescription,
+                                      presentingViewController: self)
+    }
+}
