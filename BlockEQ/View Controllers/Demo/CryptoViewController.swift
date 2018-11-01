ae06398cc6626ef674ec6e6a19b73377ea98dce4
Satraj Bambra <satraj.bambra@me.com>
2018-10-31 21:27:05 -0400

    Setup for sep6 demo using litecoin

---
+++ BlockEQ/View Controllers/Demo/CryptoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoViewController.swift
+//  BlockEQ
+//
+//  Created by Satraj Bambra on 2018-10-31.
+//  Copyright © 2018 BlockEQ. All rights reserved.
+//
+
+import UIKit
+
+class CryptoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
