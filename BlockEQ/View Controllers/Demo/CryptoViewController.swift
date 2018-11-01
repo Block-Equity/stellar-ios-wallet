1ecab8ee0e3c9bf6a2771e49c110ecfdf3b9c037
Satraj Bambra <satraj.bambra@me.com>
2018-11-01 19:18:55 -0400

    Added QR code.

--- BlockEQ/View Controllers/Demo/CryptoViewController.swift
+++ BlockEQ/View Controllers/Demo/CryptoViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class CryptoViewController: UIViewController {
-
+    
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var addressTitleLabel: UILabel!
+    @IBOutlet var addressDescriptionLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var addressHolderView: UIView!
+    @IBOutlet var imageViewHolder: UIView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var qrHolderView: UIView!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .default }
+    
+    var address: String = "LU6eDU72nL4oHSWrzSGKHjyM8EMqgTD6Xq"
+    
+    @IBAction func copyAddress() {
+        if let addressText = addressLabel.text, !addressText.isEmpty {
+            UIPasteboard.general.string = addressLabel.text
+            
+            let alert = UIAlertController(title: "ADDRESS_COPIED".localized(),
+                                          message: nil,
+                                          preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "GENERIC_OK_TEXT".localized(), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    init() {
+        super.init(nibName: String(describing: CryptoViewController.self), bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupView()
         
+        setupView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        displayGeneratedAddress(value: address)
     }
     
     func setupView() {
@@ -26,9 +67,30 @@ class CryptoViewController: UIViewController {
                                                  action: #selector(self.dismissView))
         
         navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        imageViewHolder.layer.shadowColor = Colors.shadowGray.cgColor
+        imageViewHolder.layer.shadowOpacity = Float(Alphas.transparent)
+        imageViewHolder.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        imageViewHolder.layer.shadowRadius = 4.0
+        
+        addressLabel.textColor = Colors.darkGray
+        addressTitleLabel.textColor = Colors.darkGrayTransparent
+        addressDescriptionLabel.textColor = Colors.darkGrayTransparent
+        addressHolderView.backgroundColor = Colors.lightBackground
+        view.backgroundColor = Colors.lightBackground
+        tableView.backgroundColor = Colors.lightBackground
+        qrHolderView.backgroundColor = Colors.primaryDark
+        
+        addressLabel.text = address
     }
     
     @objc func dismissView() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func displayGeneratedAddress(value: String) {
+        let map = QRMap(with: value, correctionLevel: .full)
+        imageView.image = map.scaledTemplateImage(scale: 10)
+        activityIndicator.stopAnimating()
     }
 }
