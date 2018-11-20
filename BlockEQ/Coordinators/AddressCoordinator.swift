67bc78012201a61a580baee030218bf59cb741b8
Nick DiZazzo <nick.dizazzo@gmail.com>
2018-11-20 11:06:42 -0500

    [FIX] Updated the address resolver to fetch the exchange list from the API

--- BlockEQ/Coordinators/AddressCoordinator.swift
+++ BlockEQ/Coordinators/AddressCoordinator.swift
@@ -15,21 +15,15 @@ final class AddressResolver {
     }
 
     static let shared = AddressResolver()
-
     private var mappedExchanges: [String: Exchange] = [:]
     private var mappedContacts: [String: LocalContact] = [:]
 
-    private init() {
-        loadExchangeData()
-
-        let contacts: [LocalContact] = []
-        mappedContacts = contacts.reduce(into: [:]) { map, contact in
-            map[contact.address] = contact
-        }
-    }
-
     static func lookup(address: StellarAddress) -> AddressType {
-        return .exchange
+        if let _: LocalContact = self.resolve(address: address) {
+            return .contact
+        } else {
+            return .exchange
+        }
     }
 
     static func resolve(address: StellarAddress) -> LocalContact? {
@@ -54,15 +48,38 @@ final class AddressResolver {
         return nil
     }
 
+    private func mapContactData(with list: [LocalContact]) {
+        mappedContacts = list.reduce(into: [:]) { map, contact in
+            map[contact.address] = contact
+        }
+    }
+
+    private func mapExchangeData(with list: [Exchange]) {
+        mappedExchanges = list.reduce(into: [:]) { map, exchange in
+            map[exchange.address] = exchange
+        }
+    }
+
     private func loadExchangeData() {
         let decoder = JSONDecoder()
         guard let path = Bundle.main.path(forResource: "exchanges", ofType: "json") else { return }
 
         if let localExchangeData = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe),
             let decodedExchanges = try? decoder.decode([Exchange].self, from: localExchangeData) {
-            mappedExchanges = decodedExchanges.reduce(into: [:]) { map, exchange in
-                map[exchange.address] = exchange
-            }
+            self.mapExchangeData(with: decodedExchanges)
         }
+    }
+
+    public static func fetchExchangeData() {
+        let fetchOperation = FetchExchangesOperation(completion: { exchangeList in
+            self.shared.mapExchangeData(with: exchangeList)
+        }, failure: { error in
+            print("WARNING: Failed to refresh exchange list, defaulting to local copy. (\(error))")
+            self.shared.loadExchangeData()
+        })
+
+        let operationQueue = OperationQueue()
+        operationQueue.qualityOfService = .background
+        operationQueue.addOperation(fetchOperation)
     }
 }
