4c6465c7cc1b50b4cc01694b7539036ca37d7ee9
Nick DiZazzo <nick.dizazzo@gmail.com>
2019-01-28 15:07:37 -0500

    [FEATURE] Make reusable view component for address entry

---
+++ BlockEQ/Views/Address/AddressEntryView.swift
@@ -0,0 +1,98 @@
+//
+//  AddressEntryView.swift
+//  BlockEQ
+//
+//  Created by Nick DiZazzo on 2019-01-28.
+//  Copyright © 2019 BlockEQ. All rights reserved.
+//
+
+import Reusable
+
+protocol AddressEntryViewDelegate: AnyObject {
+    func selectedPrimaryAction(_ view: AddressEntryView)
+    func selectedAddressAction(_ view: AddressEntryView)
+    func updatedAddressText(_ view: AddressEntryView, text: String?)
+    func stoppedEditingAddress(_ view: AddressEntryView, text: String?)
+}
+
+final class AddressEntryView: UIView, NibOwnerLoadable {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var addressButton: AppButton!
+    @IBOutlet weak var nextButton: AppButton!
+
+    weak var delegate: AddressEntryViewDelegate?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.loadNibContent()
+        setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.loadNibContent()
+        setupView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+
+    func setupView() {
+        self.topBorder(with: ViewColors.viewBorderColor, width: 1)
+        self.bottomBorder(with: ViewColors.viewBorderColor, width: 1)
+
+        titleLabel.textColor = TextColors.greyLabelText
+        addressTextField.textColor = TextColors.greyTextFieldText
+        addressButton.tintColor = Colors.white
+
+        titleLabel.text = "SEND_TO".localized().uppercased()
+        nextButton.setTitle("ADDRESS_ENTRY_BUTTON_LABEL".localized(), for: .normal)
+        addressButton.setImage(UIImage(named: "camera"), for: .normal)
+
+        addressTextField.placeholder = "DESTINATION_ADDRESS".localized()
+        addressTextField.clearButtonMode = .whileEditing
+    }
+
+    func update(with viewModel: ViewModel) {
+        let image = viewModel.addressButtonIcon?.withRenderingMode(.alwaysTemplate) ?? UIImage(named: "camera")
+
+        titleLabel.text = viewModel.labelText
+        addressTextField.text = viewModel.addressFieldPrefilledText
+        addressTextField.placeholder = viewModel.addressFieldPlaceholder
+
+        addressButton.setImage(image, for: .normal)
+        nextButton.setTitle(viewModel.primaryButtonTitle, for: .normal)
+    }
+}
+
+// MARK: - IBActions
+extension AddressEntryView {
+    @IBAction func finishedEditingAddress(_ sender: Any) {
+        delegate?.stoppedEditingAddress(self, text: addressTextField.text)
+    }
+
+    @IBAction func updatedAddress(_ sender: Any) {
+        delegate?.updatedAddressText(self, text: addressTextField.text)
+    }
+
+    @IBAction func selectedAddressButton(_ sender: Any) {
+        delegate?.selectedAddressAction(self)
+    }
+
+    @IBAction func selectedPrimaryButton(_ sender: Any) {
+        delegate?.selectedPrimaryAction(self)
+    }
+}
+
+extension AddressEntryView {
+    struct ViewModel {
+        let labelText: String
+        let primaryButtonTitle: String
+        let addressFieldPlaceholder: String?
+        let addressFieldPrefilledText: String?
+        let addressButtonIcon: UIImage?
+    }
+}
---
+++ BlockEQSnapshotTests/Address/AddressEntryViewSnapshotTests.swift
@@ -0,0 +1,101 @@
+//
+//  AddressEntryViewSnapshotTests.swift
+//  BlockEQSnapshotTests
+//
+//  Created by Nick DiZazzo on 2019-01-28.
+//  Copyright © 2019 BlockEQ. All rights reserved.
+//
+
+@testable import BlockEQ
+import SnapshotTesting
+import XCTest
+
+final class AddressEntryViewSnapshotTests: XCTestCase, SnapshotTest {
+    var recordMode: Bool = false
+    var defaultFrame = CGRect(x: 0, y: 0, width: 375, height: 205)
+
+    func testEmptyAddressEntryView() {
+        let entryView = AddressEntryView(frame: defaultFrame)
+
+        let viewModel = AddressEntryView.ViewModel(labelText: "SEND TO",
+                                                   primaryButtonTitle: "Make It So",
+                                                   addressFieldPlaceholder: "Placeholder Text",
+                                                   addressFieldPrefilledText: nil,
+                                                   addressButtonIcon: nil)
+
+        entryView.update(with: viewModel)
+
+        assertSnapshot(matching: entryView, as: .image, record: self.recordMode)
+    }
+
+    func testFilledAddressEntryView() {
+        let entryView = AddressEntryView(frame: defaultFrame)
+
+        let viewModel = AddressEntryView.ViewModel(labelText: "FUND DESTINATION",
+                                                   primaryButtonTitle: "Close Account",
+                                                   addressFieldPlaceholder: nil,
+                                                   addressFieldPrefilledText: "GBDBJNAZDG7UGK6XR3AQ2AAPGNTSQ3BPKBJFJMMYUIEDJ45ZFLMPH4G2",
+                                                   addressButtonIcon: nil)
+
+        entryView.update(with: viewModel)
+
+        assertSnapshot(matching: entryView, as: .image, record: self.recordMode)
+    }
+
+    func testCompressedEmptyAddressEntryView() {
+        let entryView = AddressEntryView(frame: CGRect(x: 0, y: 0, width: 320, height: 205))
+
+        let viewModel = AddressEntryView.ViewModel(labelText: "FUND DESTINATION",
+                                                   primaryButtonTitle: "Close Account",
+                                                   addressFieldPlaceholder: "Placeholder Text",
+                                                   addressFieldPrefilledText: nil,
+                                                   addressButtonIcon: nil)
+
+        entryView.update(with: viewModel)
+
+        assertSnapshot(matching: entryView, as: .image, record: self.recordMode)
+    }
+
+    func testCompressFilledAddressEntryView() {
+        let entryView = AddressEntryView(frame: CGRect(x: 0, y: 0, width: 320, height: 205))
+
+        let viewModel = AddressEntryView.ViewModel(labelText: "FUND DESTINATION",
+                                                   primaryButtonTitle: "Close Account",
+                                                   addressFieldPlaceholder: "Placeholder Text",
+                                                   addressFieldPrefilledText: "GBDBJNAZDG7UGK6XR3AQ2AAPGNTSQ3BPKBJFJMMYUIEDJ45ZFLMPH4G2",
+                                                   addressButtonIcon: nil)
+
+        entryView.update(with: viewModel)
+
+        assertSnapshot(matching: entryView, as: .image, record: self.recordMode)
+    }
+
+    func testCustomAddressIcon() {
+        let entryView = AddressEntryView(frame: defaultFrame)
+
+        let viewModel = AddressEntryView.ViewModel(labelText: "FUND DESTINATION",
+                                                   primaryButtonTitle: "Close Account",
+                                                   addressFieldPlaceholder: "Placeholder Text",
+                                                   addressFieldPrefilledText: nil,
+                                                   addressButtonIcon: UIImage(named: "send"))
+
+        entryView.update(with: viewModel)
+
+        assertSnapshot(matching: entryView, as: .image, record: self.recordMode)
+    }
+
+    func testWideLayout() {
+        let entryView = AddressEntryView(frame: CGRect(x: 0, y: 0, width: 1024, height: 205))
+
+        let viewModel = AddressEntryView.ViewModel(labelText: "FUND DESTINATION",
+                                                   primaryButtonTitle: "Close Account",
+                                                   addressFieldPlaceholder: "Placeholder Text",
+                                                   addressFieldPrefilledText: nil,
+                                                   addressButtonIcon: UIImage(named: "send"))
+
+        entryView.update(with: viewModel)
+
+        assertSnapshot(matching: entryView, as: .image, record: self.recordMode)
+    }
+}
+
